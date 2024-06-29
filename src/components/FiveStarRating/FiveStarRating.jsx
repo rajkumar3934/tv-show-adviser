427be029a6ce15f4959b00874d4011f358a531a7
KakumanuRajkumar <rajkumar.kakumanu@fovus.co>
2024-06-28 17:24:38 -0700

    updated

---
+++ src/components/FiveStarRating/FiveStarRating.jsx
@@ -0,0 +1,27 @@
+import { StarFill, StarHalf, Star as StarEmpty } from "react-bootstrap-icons"
+
+export function FiveStarRating({ rating }) {
+  const starList = [];
+
+  const starFillCount = Math.floor(rating);
+
+  const hasHalfStar = rating - parseInt(rating) >= 0.5;
+
+  const emptyStarCount = 5 - starFillCount - (hasHalfStar ? 1 : 0);
+
+
+  for (let i = 0; i < starFillCount; i++) {
+    starList.push(<StarFill key={"start-fill" + i} />);
+  }
+
+  if (hasHalfStar) {
+    starList.push(<StarHalf key={"star-half"} />);
+  }
+
+  for (let i = 0; i < emptyStarCount; i++) {
+    starList.push(<StarEmpty key={"start-empty" + i} />);
+  }
+  return <div>
+    {starList}
+  </div>
+}
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY_PARAM = '?api_key=b063b23e7b7c219d1497b9030a782eed';
+const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original';
+const SMALL_IMG_COVER_BASE_URL = 'https://image.tmdb.org/t/p/w300';
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
